642ec3d6c2e0889e25c929171b729a5d8ae20fb5
Piyaporn222 <cfsadnkufc@gmail.com>
2026-08-25 09:04:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded',function () {
+    docment.getElementById('year').innerHTML=new Date().getFullYear();
     console.log('Portfolio ของ Piyapon พร้อมแล้ว!');
 });
